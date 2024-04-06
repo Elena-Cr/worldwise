15ca5aae8b8af3f9bd1a1c6506e3c755dd5be945
Elena Cristescu <elenacristescu11@gmail.com>
2024-04-06 12:41:05 +0200

    add the cities list

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,30 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(cities, isLoading);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +38,14 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Map</p>} />
         </Route>
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+//import styles from "./CityItem.module.css";
+
+function CityItem({ city }) {
+  return <li>city</li>;
+}
+
+export default CityItem;
