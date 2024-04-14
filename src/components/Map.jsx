98d6f59c91c36fe2f126e20af96dc553afb53afc
Elena Cristescu <elenacristescu11@gmail.com>
2024-04-14 13:50:38 +0200

    add the city markers on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,9 +3,11 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../../context/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -23,13 +25,18 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              {city.cityName}
+              <br /> {city.notes}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
-      )
     </div>
   );
 }
