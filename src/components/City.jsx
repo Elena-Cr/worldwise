e3e8c1b2efd95fc40ad46a72923c1a046de02734
Elena Cristescu <elenacristescu11@gmail.com>
2024-04-21 14:08:06 +0200

    add a callback function to the getCity function in order to add it to the dependency array of the use Effect in the City component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "./City.module.css";
@@ -33,7 +32,7 @@ function City() {
     }
 
     fetchCity();
-  }, [id]);
+  }, [id, getCity]);
 
   if (!hasDisplayCity) return <Spinner />;
 
